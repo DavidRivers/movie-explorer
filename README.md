03d15b308c493ed9d2424cc940626aa0cbe29c03
David Rivers <github.com@dpg.me>
2015-04-19 19:35:14 -0400

    Initial commit with a functional Angular app, but lots of messiness needing refactoring

---
+++ README.md
@@ -0,0 +1,21 @@
+# Movie App
+
+This app is intended as a way to discover and browse movies that the user finds by title on both desktop and mobile. You should take no longer than 5 days from recieving this excercise to deliver your completed product.
+
+## What it should do
+
+* Allow a user to search for movies using the Open Movie Database API
+	* API Documentation: http://www.omdbapi.com/
+	* We recommend you use JSONP to access the API
+* Allow a user to browse through a list of results based on their search
+* Allow a user to click on a movie to view details of that movie
+				
+## What it should be
+
+* Written using Angular or a similar js library
+* A single-page application
+* Mobile responsive
+
+## TODO
+
+* Use less to inject Bootstrap hooks via mixins/extends to cleanse markup
---
+++ app.js
@@ -0,0 +1,54 @@
+var movieExplorer = angular.module('movieExplorer', ['ngResource', 'ngRoute']);
+
+movieExplorer.config(function ($routeProvider, $locationProvider) {
+	$routeProvider
+		.when('/', {
+			templateUrl: 'views/main.html',
+			controller: 'mainController'
+		})
+		.when('/details/:imdbID', {
+			templateUrl: 'views/details.html',
+			controller: 'detailController'
+		})
+		.otherwise({
+			redirectTo: '/'
+		});
+
+	$locationProvider.html5Mode(true);
+});
+
+movieExplorer.controller('mainController', function($scope, $resource, $routeParams) {
+	var search = this;
+	search.results = [];
+
+	var searchAPI = $resource('http://www.omdbapi.com/?r=json',
+		{ callback: 'JSON_CALLBACK' },
+		{ get: { method: 'JSONP' } });
+
+	// http://img.omdbapi.com/?i=tt0072890&apikey=de6cab49
+	var posterAPI = $resource('http://img.omdbapi.com/?i=tt0072890',
+		{ apikey: 'de6cab49', callback: 'JSON_CALLBACK' },
+		{ get: { method: 'JSONP' } });
+
+	search.submit = function() {
+		searchAPI.get({ s: search.query })
+			.$promise.then(function(res) {
+				search.results = res.Search;
+			});
+	};
+
+	$scope.$on('$routeChangeStart', function(next, current) { 
+		search.results = [];
+	});
+});
+
+movieExplorer.controller('detailController', function($scope, $resource, $routeParams) {
+	var detailsAPI = $resource('http://www.omdbapi.com/?r=json',
+		{ callback: 'JSON_CALLBACK', plot: 'full', tomatoes: true },
+		{ get: { method: 'JSONP' } });
+
+	detailsAPI.get({ i: $routeParams.imdbID })
+		.$promise.then(function(res) {
+			$scope.movie = res;
+		});
+});
